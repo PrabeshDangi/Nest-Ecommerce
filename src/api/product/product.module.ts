d58b4f38289a1b058107df67d22d7026c005f380
Prabesh Dangi <dangiprabesh58@gmail.com>
2024-08-30 16:39:28 +0545

    J payo tei

--- src/api/product/product.module.ts
+++ src/api/product/product.module.ts
@@ -6,17 +6,17 @@ import { ImageUploadService } from 'src/global/services/imageupload.service';
 import { HelperService } from 'src/common/helper/helper.service';
 import { SearchScheduleService } from 'src/global/services/search/searchschedule.service';
 import { SearchModule } from 'src/global/elasticsearch/search.module';
-import { SearchService } from 'src/global/elasticsearch/Search.service';
+import { SearchService } from 'src/global/elasticsearch/search.service';
 
 @Module({
-  imports: [PrismaModule,SearchModule],
+  imports: [PrismaModule, SearchModule],
   controllers: [ProductController],
   providers: [
     ProductService,
     ImageUploadService,
     HelperService,
     SearchScheduleService,
-    SearchService
+    SearchService,
   ],
 })
 export class ProductModule {}
